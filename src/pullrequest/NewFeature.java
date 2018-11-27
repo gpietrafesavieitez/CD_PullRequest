9baf84fb15294d7bb901049ef399655badb34332
gpietrafesavieitez <gpietrafesavieitez@nautilus31.danielcastelao.org>
2018-11-27 09:22:35 +0100

    First feature commit

---
+++ src/pullrequest/NewFeature.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pullrequest;
+
+/**
+ *
+ * @author gpietrafesavieitez
+ */
+public class NewFeature {
+    //Esto es un experimento muy loco
+}
